23406960fafd8b821c17dacb8a36a470a5dd0d10
Tetiana Lytvyn <Tetiana.Lytvyn@simcorp.com>
2024-02-15 17:51:02 +0200

    adding extra line - FB2

--- MyFirstHomework/MyFirstHomework/Program.cs
+++ MyFirstHomework/MyFirstHomework/Program.cs
@@ -11,6 +11,7 @@ namespace MyFirstHomework
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            Console.WriteLine("Feature branch 2 - development 1");
             Console.ReadLine();
         }
     }
